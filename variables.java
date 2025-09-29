5f975450da17e31549fce7c7d3cf6ca99c304c6e
Divyansh902 <divyansh.2428cseai21@kiet.edu>
2025-09-29 14:39:49 +0530

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,28 @@
+package basics;
+
+public class variables {
+	public static void main(String[] args) {
+		int x;//declaration of x as integer
+		x=4;//assigning 4 as value of x 
+		System.out.println(x);
+		x=500;//The value of x is iterable we can further update it by again typecasting it 
+		System.out.println(x); 
+		int a=5,b=6;
+		System.out.println(a+b);
+		System.out.println(a/b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println("The value of a is:"+a);//this + is used as separator of int and str same as %d and , in c  
+		x++;//this works as same as c 
+		System.out.println("X after increment:"+x);
+		/*this is 
+		 * used to write multi line 
+		 * comment*/
+		double c=8;
+		double z=7;
+		System.out.println(c/z);
+		
+		 
+		  
+	}
+}
