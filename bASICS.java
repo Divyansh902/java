30033a4610e8e4205068d467dea5944904ae2826
Divyansh902 <divyansh.2428cseai21@kiet.edu>
2025-09-29 14:30:14 +0530

    Create bASICS.java

---
+++ bASICS.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class bASICS {
+	public static void main(String[] args) {
+		System.out.print("Hello world");
+		//Ln after the print is used to print line and add change line or enter at the last of the line
+		System.out.println("This is first java program");
+		System.out.println("Basics");
+		System.out.println(4+5);
+		//Anything inside the  "" is treated as string and printed as same as inside the "" and if we want to prjnt the operation between the number then print without "" 
+		System.out.print("5+6");
+	}
+}
