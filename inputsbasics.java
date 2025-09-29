bd7b41ed0e99cc676584ddff92592b2716db421d
Divyansh902 <divyansh.2428cseai21@kiet.edu>
2025-09-29 14:34:03 +0530

    Create inputsbasics.java

---
+++ inputsbasics.java
@@ -0,0 +1,12 @@
+package basics;
+import java.util.Scanner;
+public class inputsbasics {
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int a=0,b = 0;
+	System.out.print("Enter two numbers:");
+	a=sc.nextInt();
+	b=sc.nextInt();
+	System.out.println(a+b);
+	}
+}
